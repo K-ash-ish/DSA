7120c34d880eecca9277765d0d4b0e698ccfe30c
K-ash-ish <57871796+K-ash-ish@users.noreply.github.com>
2022-03-16 10:24:22 +0530

    Add files via upload

---
+++ Sorting Algo/cyclicSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int cyclicSort(int arr[], int size){
+    int i=0;
+    while(i< size){
+        int correct = arr[i] - 1;
+        if(arr[i] != arr[correct]){
+            int temp;
+            temp = arr[i];
+           arr[i] = arr[correct];
+           arr[correct]= temp;
+            // cout<<"ok";
+        }
+        else {
+            i++;
+            // cout<<"not";
+        }
+        
+    }
+}
+
+int main(){
+    int arr[5] = {3,1,4,5,2};
+    cyclicSort(arr, 5);
+    for(int x : arr){
+        cout<<x<<" ";
+    }
+}
